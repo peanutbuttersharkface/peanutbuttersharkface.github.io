9c517a393c0bfb29ece8595b217deb8cc689c138
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-11-15 20:52:20 -0700

    fix

--- w3/fetch.js
+++ w3/fetch.js
@@ -1,27 +1,41 @@
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
-let results = null;
+//let results = null;
 
 async function getPokemon(url) {
-  const response = await fetch(url);
-  if (response.ok) {
-    const data = await response.json();
-    doStuff(data);
-  }
+    try{
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        doStuff(data);
+      }else {
+        throw new Error('response not ok');
+      }
+    } catch (error) {
+        console.error('error getting data:', error);
+    }
 }
 
 function doStuff(data) {
-  const results = data.results;
-  const selectElement = document.createElement('select');
-  document.body.appendChild(selectElement); 
+    try{
+     const results = data.results;
+     if(results && Array.isArray(results)){
+      const selectElement = document.createElement('select');
 
 
-  results.forEach((pokemon) => {
-    const option = document.createElement('option');
-    option.value = pokemon.name;
-    option.textContent = pokemon.name;
-    selectElement.appendChild(option);
-  });
+      results.forEach((pokemon) => {
+        const option = document.createElement('option');
+        option.value = pokemon.name;
+        option.textContent = pokemon.name;
+        selectElement.appendChild(option);
+      });
 
+      document.body.appendChild(selectElement);
+    } else {
+      throw new Error('Invalid data format or empty results array.');
+    }
+  } catch (error) {
+    console.error('Error processing data:', error);
+  }
 }
 
 getPokemon(url);
