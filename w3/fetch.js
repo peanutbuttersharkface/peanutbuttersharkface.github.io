27ce911f6845f47f666332ebca8e0a5c456e3a35
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-11-15 20:29:08 -0700

    updated js

--- w3/fetch.js
+++ w3/fetch.js
@@ -1,24 +1,26 @@
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 let results = null;
+
 async function getPokemon(url) {
   const response = await fetch(url);
-  //check to see if the fetch was successful
   if (response.ok) {
-    // the API will send us JSON...but we have to convert the response before we can use it
-    // .json() also returns a promise...so we await it as well.
     const data = await response.json();
     doStuff(data);
   }
 }
+
 function doStuff(data) {
- const results = data.results;
- const selectElement = document.createElement('select');
- results.forEach((pokemon) => {
+  const results = data.results;
+  const selectElement = document.createElement('select');
+
+  results.forEach((pokemon) => {
     const option = document.createElement('option');
     option.value = pokemon.name;
     option.textContent = pokemon.name;
     selectElement.appendChild(option);
-    
   });
+
+  document.body.appendChild(selectElement); // Move this line inside the doStuff function
 }
-document.body.appendChild(selectElement);
+
+getPokemon(url);
